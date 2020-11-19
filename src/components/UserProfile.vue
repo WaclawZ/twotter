48cd69f1b6c05ed11b4d66d9fd8121473e4941ef
WaclawZ <waclaw.zalicki@gmail.com>
2020-11-19 17:55:42 +0100

    chapter 5

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -6,6 +6,23 @@
             <div class="follower-count">
                 <strong>Followers: </strong> {{ followers }}
             </div>
+
+            <form class="create-twoot" @submit.prevent="createNewTwoot">
+                <label for="newTwoot"><strong>New Twoot</strong></label>
+                <textarea id="newTwoot" rows="4" v-model="newTwootContent"/>
+                <div class="create-twoot-type">
+                    <label for="newTwootType"><strong>Type: </strong></label>
+                    <select id="newTwootType" v-model="selectedTwootType">
+                        <option :value="option.value" v-for="(option, index) in twootTypes" :key="index">
+                            {{option.name}}
+                        </option>
+                    </select>
+                </div>
+                <button>
+                    Twoot!
+                </button>
+            </form>
+
         </div>
         <div class="twoots-wrapper">
             <TwootItem
@@ -26,7 +43,12 @@
         components: {TwootItem},
         data() {
             return {
-                inputText: "",
+                newTwootContent: '',
+                selectedTwootType: "instant",
+                twootTypes: [
+                    { value: "draft", name: "Draft"},
+                    { value: "instant", name: "Instant Twoot"},
+                ],
                 followers: 0,
                 user: {
                     id: 0,
@@ -60,6 +82,15 @@
             },
             toggleFavourite(id) {
                 console.log(`Twoot #${id}`)
+            },
+            createNewTwoot() {
+                if(this.newTwootContent && this.selectedTwootType !== "draft") {
+                    this.user.twoots.unshift({
+                        id: this.user.twoots.length,
+                        content: this.newTwootContent
+                    })
+                    this.newTwootContent = "";
+                }
             }
         },
         mounted() {
@@ -101,6 +132,12 @@
     .twoots-wrapper {
         display: grid;
         grid-gap: 10px;
+    }
+
+    .create-twoot {
+        padding-top: 10px;
+        display: flex;
+        flex-direction: column;
     }
 
 </style>
